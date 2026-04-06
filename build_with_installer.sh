b3bacadfe25d7dfe356294c7fa74edc87039a3ee
fadhil riyanto <me@fadev.org>
2026-04-06 13:22:05 +0700

    integrate tealinux-modularity into ISO file
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- build_with_installer.sh
+++ build_with_installer.sh
@@ -4,9 +4,17 @@ sudo sed -i "s|Server = file://.*|Server = file://$(pwd)/localrepo/|" ./tealinux
 start_time=$(date +%s)
 
 # Build and add installer to localrepo
+# cd ./tealinux-modularitea
+# makepkg -fs
+# mv tealinux-modularity-git-1.0-1-x86_64.pkg.tar.zst ../localrepo
+# cd ..
+
 cd ./localrepo
-makepkg -fs
-repo-add localrepo.db.tar.xz tealinux-installer-git-2.0-1-x86_64.pkg.tar.zst
+# makepkg -fs
+repo-add localrepo.db.tar.xz \
+	tealinux-installer-git-2.0-1-x86_64.pkg.tar.zst \
+	tealinux-modularity-git-1.0-1-x86_64.pkg.tar.zst
+
 cd ..
 
 # Build ISO
--- tealinux/profiledef.sh
+++ tealinux/profiledef.sh
@@ -21,7 +21,7 @@ airootfs_image_type="squashfs"
 # airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
 
 # Faster compression
-airootfs_image_tool_options=('-comp' 'zstd' '-b' '256K' '-Xcompression-level' '19')
+airootfs_image_tool_options=('-comp' 'zstd' '-b' '256K' '-Xcompression-level' '1')
 bootstrap_tarball_compression=(zstd -c -T0 --long -15)
 
 ## Fall back if above fails
